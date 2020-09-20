1c9ad31831eed161b7e39c0d72cbb16bcad514a3
amilytom <tclinbest@163.com>
2020-09-20 11:39:48 +0800

    不明白还要上传

--- build/utils.js
+++ build/utils.js
@@ -1,12 +1,12 @@
-'use strict';
-const path = require('path');
-const config = require('../config');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const packageConfig = require('../package.json');
+"use strict";
+const path = require("path");
+const config = require("../config");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const packageConfig = require("../package.json");
 
 exports.assetsPath = function (_path) {
   const assetsSubDirectory =
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === "production"
       ? config.build.assetsSubDirectory
       : config.dev.assetsSubDirectory;
 
@@ -17,17 +17,17 @@ exports.cssLoaders = function (options) {
   options = options || {};
 
   const cssLoader = {
-    loader: 'css-loader',
+    loader: "css-loader",
     options: {
-      sourceMap: options.sourceMap
-    }
+      sourceMap: options.sourceMap,
+    },
   };
 
   const postcssLoader = {
-    loader: 'postcss-loader',
+    loader: "postcss-loader",
     options: {
-      sourceMap: options.sourceMap
-    }
+      sourceMap: options.sourceMap,
+    },
   };
 
   // generate loader string to be used with extract text plugin
@@ -38,10 +38,10 @@ exports.cssLoaders = function (options) {
 
     if (loader) {
       loaders.push({
-        loader: loader + '-loader',
+        loader: loader + "-loader",
         options: Object.assign({}, loaderOptions, {
-          sourceMap: options.sourceMap
-        })
+          sourceMap: options.sourceMap,
+        }),
       });
     }
 
@@ -50,10 +50,10 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: "vue-style-loader",
       });
     } else {
-      return ['vue-style-loader'].concat(loaders);
+      return ["vue-style-loader"].concat(loaders);
     }
   }
 
@@ -61,11 +61,11 @@ exports.cssLoaders = function (options) {
   return {
     css: generateLoaders(),
     postcss: generateLoaders(),
-    less: generateLoaders('less'),
-    sass: generateLoaders('sass', {indentedSyntax: true}),
-    scss: generateLoaders('sass'),
-    stylus: generateLoaders('stylus'),
-    styl: generateLoaders('stylus')
+    less: generateLoaders("less"),
+    sass: generateLoaders("sass", { indentedSyntax: true }),
+    scss: generateLoaders("sass"),
+    stylus: generateLoaders("stylus"),
+    styl: generateLoaders("stylus"),
   };
 };
 
@@ -77,8 +77,8 @@ exports.styleLoaders = function (options) {
   for (const extension in loaders) {
     const loader = loaders[extension];
     output.push({
-      test: new RegExp('\\.' + extension + '$'),
-      use: loader
+      test: new RegExp("\\." + extension + "$"),
+      use: loader,
     });
   }
 
@@ -86,19 +86,19 @@ exports.styleLoaders = function (options) {
 };
 
 exports.createNotifierCallback = () => {
-  const notifier = require('node-notifier');
+  const notifier = require("node-notifier");
 
   return (severity, errors) => {
-    if (severity !== 'error') return;
+    if (severity !== "error") return;
 
     const error = errors[0];
-    const filename = error.file && error.file.split('!').pop();
+    const filename = error.file && error.file.split("!").pop();
 
     notifier.notify({
       title: packageConfig.name,
-      message: severity + ': ' + error.name,
-      subtitle: filename || '',
-      icon: path.join(__dirname, 'logo.png')
+      message: severity + ": " + error.name,
+      subtitle: filename || "",
+      icon: path.join(__dirname, "logo.png"),
     });
   };
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,74 +1,74 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Cookies from 'js-cookie';
+import Vue from "vue";
+import Router from "vue-router";
+import Cookies from "js-cookie";
 
 Vue.use(Router);
 
 const router = new Router({
-  mode: 'history',
-  scrollBehavior: () => ({y: 0}),
+  mode: "history",
+  scrollBehavior: () => ({ y: 0 }),
   routes: [
     {
-      path: '/',
-      redirect: '/intro'
+      path: "/",
+      redirect: "/intro",
     },
     {
-      path: '/',
-      name: 'Home',
-      component: () => import('../components/Home.vue'),
-      meta: {icon: 'el-icon-s-home', index: '/', title: '自述文件'},
+      path: "/",
+      name: "Home",
+      component: () => import("../components/Home.vue"),
+      meta: { icon: "el-icon-s-home", index: "/", title: "自述文件" },
       children: [
         {
-          path: '/intro',
-          name: 'Intro',
-          component: () => import('../views/intro.vue'),
-          meta: {icon: 'el-icon-s-home', index: 'intro', title: '系统首页'}
+          path: "/intro",
+          name: "Intro",
+          component: () => import("../views/intro.vue"),
+          meta: { icon: "el-icon-s-home", index: "intro", title: "系统首页" },
         },
         {
-          path: '/cate',
-          mame: 'Cate',
-          component: () => import('../views/cate.vue'),
-          meta: {icon: 'el-icon-s-flag', index: 'cate', title: '分类管理'}
+          path: "/cate",
+          mame: "Cate",
+          component: () => import("../views/cate.vue"),
+          meta: { icon: "el-icon-s-flag", index: "cate", title: "分类管理" },
         },
         {
-          path: '/link',
-          mame: 'Link',
-          component: () => import('../views/link.vue'),
-          meta: {icon: 'el-icon-star-on', index: 'link', title: '链接管理'}
+          path: "/link",
+          mame: "Link",
+          component: () => import("../views/link.vue"),
+          meta: { icon: "el-icon-star-on", index: "link", title: "链接管理" },
         },
         {
-          path: '/auth',
-          mame: 'Auth',
-          component: () => import('../views/auth.vue'),
-          meta: {icon: 'el-icon-s-tools', index: 'auth', title: '权限管理'}
+          path: "/auth",
+          mame: "Auth",
+          component: () => import("../views/auth.vue"),
+          meta: { icon: "el-icon-s-tools", index: "auth", title: "权限管理" },
         },
         {
-          path: '/group',
-          mame: 'Group',
-          component: () => import('../views/group.vue'),
-          meta: {icon: 'el-icon-s-order', index: 'group', title: '群组管理'}
+          path: "/group",
+          mame: "Group",
+          component: () => import("../views/group.vue"),
+          meta: { icon: "el-icon-s-order", index: "group", title: "群组管理" },
         },
         {
-          path: '/user',
-          name: 'User',
-          component: () => import('../views/user.vue'),
+          path: "/user",
+          name: "User",
+          component: () => import("../views/user.vue"),
           meta: {
-            icon: 'el-icon-user-solid',
-            index: 'user',
-            title: '用户管理'
-          }
-        }
+            icon: "el-icon-user-solid",
+            index: "user",
+            title: "用户管理",
+          },
+        },
       ],
     },
     {
-      path: '/login',
-      name: 'Login',
-      component: () => import('../views/login.vue')
+      path: "/login",
+      name: "Login",
+      component: () => import("../views/login.vue"),
     },
     {
-      path: '*',
-      redirect: '/404'
-    }
+      path: "*",
+      redirect: "/404",
+    },
   ],
 });
 
@@ -82,9 +82,9 @@ export default router;
 
 //使用钩子函数对路由进行权限跳转
 router.beforeEach((to, from, next) => {
-  const token = Cookies.get('access-token');
-  if (!token && to.path !== '/login') {
-    next('/login');
+  const token = Cookies.get("access-token");
+  if (!token && to.path !== "/login") {
+    next("/login");
   }
   next();
 });
